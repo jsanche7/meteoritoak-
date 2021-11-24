568a261fe16fc19fd3f758bec8f6c8f34a90945b
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-11-24 16:18:14 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,25 +4,22 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     Jokalaria.move(1)
 })
-let Meteorito2: game.LedSprite = null
-let Meteorito: game.LedSprite = null
+let Pilota: game.LedSprite = null
 let Jokalaria: game.LedSprite = null
 Jokalaria = game.createSprite(2, 4)
 basic.forever(function () {
-    basic.pause(randint(300, 600))
-    Meteorito = game.createSprite(randint(0, 4), 0)
-    Meteorito2 = game.createSprite(randint(0, 4), 0)
+    basic.pause(randint(500, 1000))
+    Pilota = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
-        basic.pause(80)
-        Meteorito.change(LedSpriteProperty.Y, 1)
-        Meteorito2.change(LedSpriteProperty.Y, 1)
+        basic.pause(400)
+        Pilota.change(LedSpriteProperty.Y, 1)
     }
-    if (Meteorito.isTouching(Jokalaria) || Meteorito2.isTouching(Jokalaria)) {
-        music.playMelody("G E C - G E C - ", 800)
-        game.gameOver()
+    if (Pilota.get(LedSpriteProperty.Y) == 4) {
+        if (Jokalaria.isTouching(Pilota)) {
+            game.addScore(1)
+            Pilota.delete()
+        } else {
+            game.gameOver()
+        }
     }
-    basic.pause(200)
-    music.playMelody("C E G C E G - - ", 800)
-    game.addScore(1)
-    Meteorito.delete()
 })
