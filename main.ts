6bd4576951ee509ab05031b61915768acc1e37f1
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-11-24 18:03:18 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,23 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     Jokalaria.move(1)
 })
-let Meteorito: game.LedSprite = null
+let Pilota: game.LedSprite = null
 let Jokalaria: game.LedSprite = null
 Jokalaria = game.createSprite(2, 4)
 basic.forever(function () {
-    basic.pause(randint(500, 750))
-    Meteorito = game.createSprite(randint(0, 4), 0)
+    basic.pause(randint(1000, 1500))
+    Pilota = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
-        basic.pause(100)
-        Meteorito.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+        Pilota.change(LedSpriteProperty.Y, 1)
     }
-    if (Meteorito.isTouching(Jokalaria)) {
-        game.gameOver()
+    if (Pilota.get(LedSpriteProperty.Y) == 4) {
+        if (Pilota.isTouching(Jokalaria)) {
+            game.addScore(1)
+            Pilota.delete()
+        } else {
+            game.gameOver()
+            basic.pause(200)
+        }
     }
-    basic.pause(200)
-    game.addScore(1)
-    Meteorito.delete()
 })
